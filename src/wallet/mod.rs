76803cf060c2874134e5778c80e19ac9e449507e
Roy Murphy <roycoins@gmail.com>
2025-10-12 21:17:21 +0100

    Create mod.rs

---
+++ src/wallet/mod.rs
@@ -0,0 +1,16 @@
+//! Wallet module for Bitcoin SV key management (BIP-32 extended keys).
+
+pub mod extended_key;
+pub use crate::address;
+
+pub use self::extended_key::{
+    ExtendedKey,
+    ExtendedKeyType,
+    HARDENED_KEY,
+    MAINNET_PRIVATE_EXTENDED_KEY,
+    MAINNET_PUBLIC_EXTENDED_KEY,
+    TESTNET_PRIVATE_EXTENDED_KEY,
+    TESTNET_PUBLIC_EXTENDED_KEY,
+    derive_extended_key,
+    extended_key_from_seed,
+};
