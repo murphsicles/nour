c8241ca520371f4cf3d1a227b6d61360b9e3cfbc
Roy Murphy <roycoins@gmail.com>
2025-10-20 22:08:05 +0100

    Update documentation for P2PKH transaction signing

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,30 +1,31 @@
 //! Transaction building and signing for Bitcoin SV.
+
 ///
 /// Supports P2PKH/P2SH, sighash computation (BIP-143 + forkid), and ECDSA signing.
 ///
 /// # Examples
-//!
-//! Sign a P2PKH transaction:
-//! ```
-//! use nour::messages::{Tx, TxIn};
-//! use nour::transaction::{generate_signature, p2pkh::{create_lock_script, create_unlock_script}, sighash::{sighash, SigHashCache, SIGHASH_FORKID, SIGHASH_NONE}};
-//! use nour::util::hash160;
-//!
-//! // Use real values
-//! let mut tx = Tx {
-//!     inputs: vec![TxIn { ..Default::default() }],
-//!     ..Default::default()
-//! };
-//! let private_key = [1; 32];
-//! let public_key = [1; 33];
-//!
-//! let lock_script = create_lock_script(&hash160(&public_key));
-//! let mut cache = SigHashCache::new();
-//! let sighash_type = SIGHASH_NONE | SIGHASH_FORKID;
-//! let sighash_val = sighash(&tx, 0, &lock_script.0, 0, sighash_type, &mut cache).unwrap();
-//! let signature = generate_signature(&private_key, &sighash_val, sighash_type).unwrap();
-//! tx.inputs[0].unlock_script = create_unlock_script(&signature, &public_key);
-//! ```
+///
+/// Sign a P2PKH transaction:
+/// ```
+/// use nour::messages::{Tx, TxIn};
+/// use nour::transaction::{generate_signature, p2pkh::{create_lock_script, create_unlock_script}, sighash::{sighash, SigHashCache, SIGHASH_FORKID, SIGHASH_NONE}};
+/// use nour::util::hash160;
+///
+/// // Use real values
+/// let mut tx = Tx {
+///     inputs: vec![TxIn { ..Default::default() }],
+///     ..Default::default()
+/// };
+/// let private_key = [1; 32];
+/// let public_key = [1; 33];
+///
+/// let lock_script = create_lock_script(&hash160(&public_key));
+/// let mut cache = SigHashCache::new();
+/// let sighash_type = SIGHASH_NONE | SIGHASH_FORKID;
+/// let sighash_val = sighash(&tx, 0, &lock_script.0, 0, sighash_type, &mut cache).unwrap();
+/// let signature = generate_signature(&private_key, &sighash_val, sighash_type).unwrap();
+/// tx.inputs[0].unlock_script = create_unlock_script(&signature, &public_key);
+/// ```
 pub mod p2pkh;
 pub mod sighash;
 
