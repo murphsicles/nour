173db6eae624241b468f2d8bab2089bd380159bd
Roy Murphy <roycoins@gmail.com>
2025-10-25 16:44:22 +0100

    Refactor OutPoint struct and implement Serializable

--- src/messages/out_point.rs
+++ src/messages/out_point.rs
@@ -1,18 +1,15 @@
 //! OutPoint for Bitcoin SV P2P, referencing transaction outputs.
 
-use crate::util::{Hash256, Result, Serializable};
+use crate::util::{Error, Hash256, Result, Serializable};
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use std::io;
 use std::io::{Read, Write};
-
 #[cfg(feature = "async")]
 use tokio::io::{AsyncRead, AsyncWrite};
-
 /// The coinbase transaction input hash (all zeros).
 pub const COINBASE_OUTPOINT_HASH: Hash256 = Hash256([0; 32]);
 /// The coinbase transaction input index (0xffffffff).
 pub const COINBASE_OUTPOINT_INDEX: u32 = 0xffffffff;
-
 /// Reference to a transaction output.
 #[derive(Debug, Default, PartialEq, Eq, Hash, Clone)]
 pub struct OutPoint {
@@ -21,11 +18,9 @@ pub struct OutPoint {
     /// Index of the output in the transaction, zero-indexed.
     pub index: u32,
 }
-
 impl OutPoint {
     /// Size of the out point in bytes (32 + 4 = 36).
     pub const SIZE: usize = 36;
-
     /// Returns the size of the out point in bytes.
     #[must_use]
     #[inline]
@@ -33,7 +28,6 @@ impl OutPoint {
         Self::SIZE
     }
 }
-
 impl Serializable<OutPoint> for OutPoint {
     fn read(reader: &mut dyn Read) -> Result<OutPoint> {
         let hash = Hash256::read(reader)?;
@@ -42,13 +36,11 @@ impl Serializable<OutPoint> for OutPoint {
         let index = u32::from_le_bytes(index);
         Ok(OutPoint { hash, index })
     }
-
     fn write(&self, writer: &mut dyn Write) -> io::Result<()> {
         self.hash.write(writer)?;
         writer.write_all(&self.index.to_le_bytes())
     }
 }
-
 #[cfg(feature = "async")]
 impl AsyncSerializable<OutPoint> for OutPoint {
     async fn read_async(reader: &mut dyn AsyncRead) -> Result<OutPoint> {
@@ -58,26 +50,23 @@ impl AsyncSerializable<OutPoint> for OutPoint {
         let index = u32::from_le_bytes(index);
         Ok(OutPoint { hash, index })
     }
-
     async fn write_async(&self, writer: &mut dyn AsyncWrite) -> io::Result<()> {
         self.hash.write_async(writer).await?;
         writer.write_all(&self.index.to_le_bytes()).await
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use hex;
     use std::io::Cursor;
     use pretty_assertions::assert_eq;
-
     #[test]
     fn write_read() {
         let mut v = Vec::new();
         let t = OutPoint {
             hash: Hash256::decode(
-                "123412345678567890ab90abcdefcdef123412345678567890ab90abcdefcdef",
+                "123412345678567890ab9090abcdefcdef123412345678567890ab90abcdefcdef",
             )
             .unwrap(),
             index: 0,
@@ -86,7 +75,6 @@ mod tests {
         assert_eq!(v.len(), t.size());
         assert_eq!(OutPoint::read(&mut Cursor::new(&v)).unwrap(), t);
     }
-
     #[test]
     fn coinbase() {
         let mut v = Vec::new();
