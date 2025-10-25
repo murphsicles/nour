477ba8484f77ba70f847a0d4202e05c878237a23
Roy Murphy <roycoins@gmail.com>
2025-10-25 14:05:34 +0100

    Refactor serialization traits and fix type usage

--- src/util/serdes.rs
+++ src/util/serdes.rs
@@ -1,12 +1,9 @@
 //! Binary serialization/deserialization trait for Bitcoin SV objects.
-
-use crate::util::Result;
+use crate::util::{Error, Result};
 use std::io;
 use std::io::{Read, Write};
-
 #[cfg(feature = "async")]
 use tokio::io::{AsyncRead, AsyncWrite};
-
 /// An object that may be serialized and deserialized.
 pub trait Serializable<T> {
     /// Reads the object from serialized form.
@@ -16,14 +13,12 @@ pub trait Serializable<T> {
     fn read(reader: &mut dyn Read) -> Result<T>
     where
         Self: Sized;
-
-    /// Writes the object to serialized form.
+    /// Writes the object to the object to serialized form.
     ///
     /// # Errors
     /// IO errors.
     fn write(&self, writer: &mut dyn Write) -> io::Result<()>;
 }
-
 #[cfg(feature = "async")]
 pub trait AsyncSerializable<T> {
     /// Reads the object from serialized form asynchronously.
@@ -33,38 +28,32 @@ pub trait AsyncSerializable<T> {
     async fn read_async(reader: &mut dyn AsyncRead) -> Result<T>
     where
         Self: Sized;
-
     /// Writes the object to serialized form asynchronously.
     ///
     /// # Errors
     /// IO errors.
     async fn write_async(&self, writer: &mut dyn AsyncWrite) -> io::Result<()>;
 }
-
 impl Serializable<[u8; 16]> for [u8; 16] {
     fn read(reader: &mut dyn Read) -> Result<[u8; 16]> {
         let mut d = [0; 16];
         reader.read_exact(&mut d).map_err(|e| Error::IOError(e))?;
         Ok(d)
     }
-
     fn write(&self, writer: &mut dyn Write) -> io::Result<()> {
         writer.write_all(self)
     }
 }
-
 impl Serializable<[u8; 32]> for [u8; 32] {
     fn read(reader: &mut dyn Read) -> Result<[u8; 32]> {
         let mut d = [0; 32];
         reader.read_exact(&mut d).map_err(|e| Error::IOError(e))?;
         Ok(d)
     }
-
     fn write(&self, writer: &mut dyn Write) -> io::Result<()> {
         writer.write_all(self)
     }
 }
-
 #[cfg(feature = "async")]
 impl AsyncSerializable<[u8; 16]> for [u8; 16] {
     async fn read_async(reader: &mut dyn AsyncRead) -> Result<[u8; 16]> {
@@ -73,12 +62,10 @@ impl AsyncSerializable<[u8; 16]> for [u8; 16] {
         reader.read_exact(&mut d).await.map_err(|e| Error::IOError(e))?;
         Ok(d)
     }
-
     async fn write_async(&self, writer: &mut dyn AsyncWrite) -> io::Result<()> {
         writer.write_all(self).await
     }
 }
-
 #[cfg(feature = "async")]
 impl AsyncSerializable<[u8; 32]> for [u8; 32] {
     async fn read_async(reader: &mut dyn AsyncRead) -> Result<[u8; 32]> {
@@ -86,41 +73,36 @@ impl AsyncSerializable<[u8; 32]> for [u8; 32] {
         reader.read_exact(&mut d).await.map_err(|e| Error::IOError(e))?;
         Ok(d)
     }
-
     async fn write_async(&self, writer: &mut dyn AsyncWrite) -> io::Result<()> {
         writer.write_all(self).await
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::io::Cursor;
     use pretty_assertions::assert_eq;
-
     #[test]
     fn test_serdes_array16() -> Result<()> {
         let array = [1; 16];
         let mut v = Vec::new();
         array.write(&mut v)?;
-        let deserialized = [u8; 16]::read(&mut Cursor::new(&v))?;
+        let deserialized = <[u8; 16]>::read(&mut Cursor::new(&v))?;
         assert_eq!(array, deserialized);
         Ok(())
     }
-
     #[test]
     fn test_serdes_array32() -> Result<()> {
         let array = [2; 32];
         let mut v = Vec::new();
         array.write(&mut v)?;
-        let deserialized = [u8; 32]::read(&mut Cursor::new(&v))?;
+        let deserialized = <[u8; 32]>::read(&mut Cursor::new(&v))?;
         assert_eq!(array, deserialized);
         Ok(())
     }
-
     #[test]
     fn test_short_read() {
         let mut cursor = Cursor::new(vec![0; 10]);
-        assert_eq!([u8; 16]::read(&mut cursor).unwrap_err().to_string(), "IO error: UnexpectedEof");
+        assert_eq!(<[u8; 16]>::read(&mut cursor).unwrap_err().to_string(), "IO error: UnexpectedEof");
     }
 }
