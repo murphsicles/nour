075f54465d93a38aa090b72c3f52b81d72218591
Roy Murphy <roycoins@gmail.com>
2025-10-26 16:46:53 +0000

    Update hash256.rs

--- src/util/hash256.rs
+++ src/util/hash256.rs
@@ -1,16 +1,18 @@
 //! 256-bit hash for blocks and transactions in Bitcoin SV.
-//
+///
 /// It is interpreted as a single little-endian number for display.
 use crate::util::{Error, Result, Serializable};
-use bitcoin_hashes::sha256d as bh_sha256d; // SIMD opt
+use bitcoin_hashes::sha256d::{Hash as Sha256dHash, sha256d as bh_sha256d};
 use hex;
 use std::cmp::Ordering;
 use std::fmt;
 use std::io;
 use std::io::{Read, Write};
+
 /// 256-bit hash for blocks and transactions.
 #[derive(Default, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Hash256(pub [u8; 32]);
+
 impl Hash256 {
     /// Converts the hash into a hex string.
     #[must_use]
@@ -20,6 +22,7 @@ impl Hash256 {
         r.reverse();
         hex::encode(r)
     }
+
     /// Converts a string of 64 hex characters into a hash.
     #[must_use]
     #[inline]
@@ -34,16 +37,25 @@ impl Hash256 {
         Ok(Hash256(hash_bytes))
     }
 }
+
+impl From<Sha256dHash> for Hash256 {
+    fn from(hash: Sha256dHash) -> Self {
+        Self(hash.to_byte_array())
+    }
+}
+
 impl Serializable<Hash256> for Hash256 {
     fn read(reader: &mut dyn Read) -> Result<Hash256> {
         let mut bytes = [0; 32];
         reader.read_exact(&mut bytes).map_err(|e| Error::IOError(e))?;
         Ok(Hash256(bytes))
     }
+
     fn write(&self, writer: &mut dyn Write) -> io::Result<()> {
         writer.write_all(&self.0)
     }
 }
+
 /// Hashes a data array twice using SHA256.
 #[must_use]
 #[inline]
@@ -51,6 +63,7 @@ pub fn sha256d(data: &[u8]) -> Hash256 {
     let h = bh_sha256d::Hash::hash(data).to_byte_array();
     Hash256(h)
 }
+
 impl Ord for Hash256 {
     fn cmp(&self, other: &Hash256) -> Ordering {
         for i in (0..32).rev() {
@@ -62,27 +75,32 @@ impl Ord for Hash256 {
         Ordering::Equal
     }
 }
+
 impl PartialOrd for Hash256 {
     fn partial_cmp(&self, other: &Hash256) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
+
 impl fmt::Debug for Hash256 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.encode())
     }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::io::Cursor;
     use pretty_assertions::assert_eq;
+
     #[test]
     fn sha256d_test() {
         let x = hex::decode("0123456789abcdef").unwrap();
         let e = hex::encode(sha256d(&x).0);
         assert_eq!(e, "137ad663f79da06e282ed0abbec4d70523ced5ff8e39d5c2e5641d978c5925aa");
     }
+
     #[test]
     fn hash_decode() {
         // Valid
@@ -100,6 +118,7 @@ mod tests {
         assert!(Hash256::decode(s2).is_err());
         assert!(Hash256::decode(s3).is_err());
     }
+
     #[test]
     fn hash_decode_write_read_encode() {
         let s1 = "abcdef0000112233445566778899abcdef000011223344556677889912345678";
@@ -110,6 +129,7 @@ mod tests {
         let s2 = h2.encode();
         assert_eq!(s1, s2);
     }
+
     #[test]
     fn hash_compare() {
         let s1 = "5555555555555555555555555555555555555555555555555555555555555555";
