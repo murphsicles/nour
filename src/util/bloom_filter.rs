b58dfb53bab6dd351d5026d2468ba567daef3417
Roy Murphy <roycoins@gmail.com>
2025-10-26 15:32:21 +0000

    Update bloom_filter.rs

--- src/util/bloom_filter.rs
+++ src/util/bloom_filter.rs
@@ -48,7 +48,8 @@ impl BloomFilter {
         let size = (-1.0 / ln2.powi(2) * insert * pr_false_pos.ln()) / 8.0;
         let size = size.min(BLOOM_FILTER_MAX_FILTER_SIZE as f64).ceil() as usize;
         let num_hash_funcs = ((size as f64 * 8.0 / insert * ln2).min(BLOOM_FILTER_MAX_HASH_FUNCS as f64)).ceil() as usize;
-        let tweak = OsRng.gen::<u32>();
+        let mut rng = OsRng;
+        let tweak = rng.gen::<u32>();
         Ok(BloomFilter {
             filter: vec![0; size],
             num_hash_funcs,
