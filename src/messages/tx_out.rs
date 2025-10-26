87c5b5b99d64bbeb8d4055181622d7dd27719434
Roy Murphy <roycoins@gmail.com>
2025-10-26 14:41:19 +0000

    Update tx_out.rs

--- src/messages/tx_out.rs
+++ src/messages/tx_out.rs
@@ -2,7 +2,7 @@
 
 use crate::script::Script;
 use crate::util::{var_int, Error, Result, Serializable};
-use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use byteorder::WriteBytesExt;
 use std::io;
 use std::io::{Read, Write};
 #[cfg(feature = "async")]
