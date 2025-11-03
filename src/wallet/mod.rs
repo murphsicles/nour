0030852c6766f79d5d5a25709b82805e7afc873f
Roy Murphy <roycoins@gmail.com>
2025-11-03 04:30:48 +0000

    Update mod.rs

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -4,13 +4,7 @@ pub mod extended_key;
 pub use crate::address;
 
 pub use self::extended_key::{
-    ExtendedKey,
-    ExtendedKeyType,
-    HARDENED_KEY,
-    MAINNET_PRIVATE_EXTENDED_KEY,
-    MAINNET_PUBLIC_EXTENDED_KEY,
-    TESTNET_PRIVATE_EXTENDED_KEY,
-    TESTNET_PUBLIC_EXTENDED_KEY,
-    derive_extended_key,
-    extended_key_from_seed,
+    ExtendedKey, ExtendedKeyType, HARDENED_KEY, MAINNET_PRIVATE_EXTENDED_KEY,
+    MAINNET_PUBLIC_EXTENDED_KEY, TESTNET_PRIVATE_EXTENDED_KEY, TESTNET_PUBLIC_EXTENDED_KEY,
+    derive_extended_key, extended_key_from_seed,
 };
